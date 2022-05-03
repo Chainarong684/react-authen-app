070f33ee17f84d95a493b5e75f0fadc20978e3e9
CKDEV_MAC2012 <9k.chainarong@gmail.com>
2022-05-03 20:26:49 +0700

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { Layout } from 'antd'
 
 import Home from './components/pages/Home'
 import Login from './components/pages/Login'
 import Register from './components/pages/Register'
+import Header from './components/layouts/Header'
+import Footer from './components/layouts/Footer'
 
 const App = () => {
+  const { Content } = Layout
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Layout>
+        <Header />
+        <Content>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Content>
+        <Footer />
+      </Layout>
     </div>
   )
 }
