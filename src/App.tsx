48977e59d2ec113f56a164b9ab7c99b56b0cbc80
CKDEV_MAC2012 <9k.chainarong@gmail.com>
2022-05-03 13:50:58 +0700

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './components/pages/Home'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
 
 const App = () => {
-  return <div></div>;
-};
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  )
+}
 
-export default App;
+export default App
